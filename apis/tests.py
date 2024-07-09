ebf9c004027d6972ae9df8d01741a9809478e445
Faith Kariuki <tcwamboi@gmail.com>
2024-07-09 19:13:31 +0300

    Add tests to API POST method

--- apis/tests.py
+++ apis/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from .models import Item
 from django.utils import timezone
 import datetime
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
 
 class ItemAPIGETTestCase(TestCase):
@@ -18,3 +21,24 @@ class ItemAPIGETTestCase(TestCase):
     def test_updated_field(self):
         self.assertTrue(isinstance(self.test_item.updated, datetime.datetime))
         self.assertTrue(self.test_item.updated <= timezone.now())
+
+
+class ItemAPIPOSTTestCase(APITestCase):
+    def test_create_item(self):
+        url = reverse('apis:post_items')
+        data = {
+            'name': 'Test Item',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.get().name, 'Test Item')
+
+    def test_post_item_invalid_data(self):
+        url = reverse('apis:post_items')
+        data = {
+            'name': '',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Item.objects.count(), 0)
--- apis/views.py
+++ apis/views.py
@@ -28,7 +28,7 @@ def post_items(request):
     serializer = ItemSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
