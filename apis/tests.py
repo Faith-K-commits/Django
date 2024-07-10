b8053c2324bab4a1e5a7973a811c0c3fd48735c3
Faith Kariuki <tcwamboi@gmail.com>
2024-07-10 15:58:29 +0300

    Add DELETE and PATCH API test cases

--- apis/tests.py
+++ apis/tests.py
@@ -42,3 +42,46 @@ class ItemAPIPOSTTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Item.objects.count(), 0)
+
+
+class ItemAPIPATCHTestCase(APITestCase):
+    def setUp(self):
+        self.item = Item.objects.create(name='Initial Item')
+
+    def test_patch_item_success(self):
+        url = reverse('apis:patch_item', args=[self.item.id])
+        data = {'name': 'Updated Item'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Item.objects.get(pk=self.item.id).name, 'Updated Item')
+
+    def test_patch_item_not_found(self):
+        url = reverse('apis:patch_item', args=[999])  # Non-existent item ID
+        data = {'name': 'Updated Item'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data, {'error': 'Item not found.'})
+
+    def test_patch_item_invalid_data(self):
+        url = reverse('apis:patch_item', args=[self.item.id])
+        data = {'name': ''}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('name', response.data)
+
+
+class ItemAPIDeleteTestCase(APITestCase):
+    def setUp(self):
+        self.item = Item.objects.create(name='Item to be deleted')
+
+    def test_delete_item_success(self):
+        url = reverse('apis:delete_items', args=[self.item.id])  
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_delete_item_not_found(self):
+        url = reverse('apis:delete_items', args=[999])  # Non-existent item ID
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data, {'error': 'Item not found.'})
